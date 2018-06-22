23783b094ee03e5113387dbe36630ad29671ff5e
triadfirewood@gmail.com <triadfirewood@gmail.com>
2018-06-22 17:55:55 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {
+  garth:"A Dream is Like a River",
+  taylor: "Never Grow Up",
+  bonJovi: "Thank you for Loving Me"
+  
+}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  return playlist 
+}
